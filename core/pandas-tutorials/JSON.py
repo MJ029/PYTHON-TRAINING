c334c890e4fcbefd98b5fc73645ef2819e3e2268
Jayasurya S <jsurya2552000@gmail.com>
2022-07-18 17:44:18 +0530

    changes

--- core/pandas-tutorials/JSON.py
+++ core/pandas-tutorials/JSON.py
@@ -1,13 +1,14 @@
 import pandas as pd
 import os
 
-def create_non_existing_dir(dir_path=str):
+
+def create_non_existing_dir(dir_path: str):
     abs_path = os.path.abspath(dir_path)
     os.makedirs(abs_path)
     return abs_path
 
-#convert Excel to JSON file
 
+# convert Excel to JSON file
 file_path = "../../dataset/File-Handling/Excel-Reading/Data-Generation.xlsx"
 target_path = "../../dataset/output"
 file_name = "Claims-History"
@@ -21,4 +22,3 @@ target_file_abs_path = create_non_existing_dir(dir_path=target_file_path)
 complete_file_path = "/".join([target_file_abs_path, f"{file_name}.json"])
 
 df.to_json(complete_file_path)
-
