1cfe4e160802a46244616de23560abcb4ff3ff4b
Jayasurya S <jsurya2552000@gmail.com>
2022-07-18 18:06:46 +0530

    pip changed

--- core/pandas-tutorials/read-csv.py
+++ core/pandas-tutorials/read-csv.py
@@ -1,11 +1,12 @@
 import pandas as pd
 
-#Filter NA records and write as CSV file
+
+# Filter NA records and write as CSV file
 
 file_path = "../../dataset/File-Handling/CSV-Reading/Employee-Master/Employee-Master.csv"
 
 df = pd.read_csv(file_path)
 
-cleansed_df = df.dropna(axis = 0, how='all')
+cleansed_df = df.dropna(axis=0, how='all')
 
 print(cleansed_df)
