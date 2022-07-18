536d83abf21f1bf9a71f9e011a81841caa38342c
Jayasurya S <jsurya2552000@gmail.com>
2022-07-18 17:59:02 +0530

    pip changed

--- core/pandas-tutorials/NumPy.py
+++ core/pandas-tutorials/NumPy.py
@@ -2,17 +2,15 @@ import pandas as pd
 import numpy as np
 
 
-#Create dataframe from numpy array
+# Create dataframe from numpy array
+wickets = np.array([[1, 1, 1], [3, 2, 1], [4, 1, 2],
+                    [1, 2, 3], [2, 3, 2], [1, 1, 1],
+                    [3, 4, 1]])
 
-wickets = np.array([[1,1,1], [3,2,1], [4,1,2],
-                   [1,2,3], [2,3,2], [1,1,1],
-                   [3,4,1]])
-
-players = ['Rahul', 'Dinesh', 'Dravid', 'Kohli', 'Shreyas', 'Balaji', 'Karthik']
+players = ['Rahul', 'Dinesh', 'Zaheer', 'Anil', 'Akthar', 'Balaji', 'Karthik']
 
 matches = ['match1', 'match2', 'match3']
 
 df = pd.DataFrame(data=wickets, index=players, columns=matches)
 
 print(df)
-
