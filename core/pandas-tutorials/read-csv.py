9af37fb9fbcdbe3e31418cd4640e1aa9d3cab85b
Jayasurya S <jsurya2552000@gmail.com>
2022-07-19 16:43:18 +0530

    changes for CSV-EXCEL

--- core/pandas-tutorials/read-csv.py
+++ core/pandas-tutorials/read-csv.py
@@ -1,12 +1,31 @@
 import pandas as pd
+import os
 
 
-# Filter NA records and write as CSV file
+def create_non_existing_dir(dir_path: str):
+    abs_path = os.path.abspath(dir_path)
+    if os.path.isdir(dir_path):
+        print("Path Exists...! Continue...")
+    else:
+        print("Path Does Not Exists...! Creating the Directory.")
+        os.makedirs(abs_path)
+        return abs_path
 
-file_path = "../../dataset/File-Handling/CSV-Reading/Employee-Master/Employee-Master.csv"
 
-df = pd.read_csv(file_path)
+# convert csv to Excel file
+file_path = "../../dataset/File-Handling/CSV-Reading/Claims-History/Claims-History.csv"
 
-cleansed_df = df.dropna(axis=0, how='all')
+target_path = "../../dataset/output"
 
-print(cleansed_df)
+file_name = "Claims-History"
+
+excel_filename = "Claims-History.xlsx"
+
+if __name__ == "__main__":
+    df = pd.read_csv(file_path)
+
+    target_file_path = "/".join([target_path, "CSV-EXCEL", file_name])
+
+    target_file_abs_path = create_non_existing_dir(dir_path=target_file_path)
+
+    complete_file_path = "/".join([target_file_abs_path, f"{file_name}.xlsx"])
