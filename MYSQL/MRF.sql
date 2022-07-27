b9bcb8829820a549f602dea62ead75716b865a1c
Jayasurya S <jsurya2552000@gmail.com>
2022-07-27 11:37:12 +0530

    FORMATTED SCRIPT

--- MYSQL/MRF.sql
+++ MYSQL/MRF.sql
@@ -1,14 +1,13 @@
 USE sql_learning;
-
 -- MRF - MOST REGISTERED FIELD -- 
-
-SELECT education AS MRF,
-	COUNT(education) AS `customers` 
-    FROM table_n
-    GROUP BY education
-    ORDER BY `customers` DESC
-    LIMIT 1;
-    
-
-    
-
+SELECT 
+  education AS MRF, 
+  COUNT(education) AS `customers` 
+FROM 
+  customer_campaign 
+GROUP BY 
+  education 
+ORDER BY 
+  `customers` DESC 
+LIMIT 
+  1;
--- MYSQL/RANK-TASKS.sql
+++ MYSQL/RANK-TASKS.sql
@@ -1,9 +1,22 @@
-SELECT ID,Income,  
-DENSE_RANK() OVER (ORDER BY Income) TOP_3_RANK  
-FROM table_n
-LIMIT 3;  
-
-SELECT ID,Income,  
-DENSE_RANK() OVER (ORDER BY Income DESC) TOP_3_RANK  
-FROM table_n
-LIMIT 3; 
+SELECT 
+  ID, 
+  Income, 
+  DENSE_RANK() OVER (
+    ORDER BY 
+      Income
+  ) TOP_3_RANK 
+FROM 
+  customer_campaign 
+LIMIT 
+  3;
+SELECT 
+  ID, 
+  Income, 
+  DENSE_RANK() OVER (
+    ORDER BY 
+      Income DESC
+  ) TOP_3_RANK 
+FROM 
+  customer_campaign 
+LIMIT 
+  3;
