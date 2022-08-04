de4482ca1adaaa2e7a6e441d92b2c7127dba80f7
Jayasurya S <jsurya2552000@gmail.com>
2022-08-04 14:25:33 +0530

    20_AcceptedCmp2

--- core/CPA/a.py
+++ core/CPA/a.py
@@ -13,7 +13,7 @@ def create_non_existing_dir(dir_path: str):
     return abs_path
 
 
-file_name = "20[AcceptedCmp1]"
+file_name = "20[AcceptedCmp2]"
 target_path = "../../PANDAS_SQL"
 
 
@@ -21,7 +21,7 @@ if __name__ == "__main__":
 
     connection = ctr.connect(user='root', host='localhost', password='blackforest', database='sql_learning')
 
-    query = "SELECT COUNT(ID) as customers, AcceptedCmp1, MntMeatProducts, MntFishProducts, MntSweetProducts, MntGoldProds FROM customer_campaign GROUP BY AcceptedCmp1;"
+    query = "SELECT COUNT(ID) as customers, AcceptedCmp2, MntMeatProducts, MntFishProducts, MntSweetProducts, MntGoldProds FROM customer_campaign GROUP BY AcceptedCmp2;"
 
     df = pd.read_sql(sql=query,con=connection)
 
