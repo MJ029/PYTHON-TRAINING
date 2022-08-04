d7df6945790138ed43355e4b0562f124d49a093c
Jayasurya S <jsurya2552000@gmail.com>
2022-08-04 14:24:34 +0530

    20[AcceptedCmp1]

--- MYSQL/19.sql
+++ MYSQL/19.sql
@@ -2,20 +2,4 @@ USE sql_learning;
 
 -- Customer with Maximum number of Web Purchase and Recently logged-in --
 
-SELECT
-  *
-FROM 
-  (
-    SELECT 
-      RANK() OVER(
-        ORDER BY 
-          NumWebPurchases DESC, Recency DESC
-      ) AS TOP_3_customers, 
-      ID, 
-      Recency,
-      NumWebPurchases
-    FROM 
-      customer_campaign
-  ) AS tmp 
-WHERE 
-  tmp.TOP_3_customers <= 3;
+SELECT * FROM ( SELECT RANK() OVER( ORDER BY NumDealsPurchases DESC, Income DESC ) AS TOP_3_customers, ID, Income, NumDealsPurchases FROM customer_campaign ) AS tmp WHERE tmp.TOP_3_customers <= 3;
--- core/CPA/a.py
+++ core/CPA/a.py
@@ -13,7 +13,7 @@ def create_non_existing_dir(dir_path: str):
     return abs_path
 
 
-file_name = "19"
+file_name = "20[AcceptedCmp1]"
 target_path = "../../PANDAS_SQL"
 
 
@@ -21,7 +21,8 @@ if __name__ == "__main__":
 
     connection = ctr.connect(user='root', host='localhost', password='blackforest', database='sql_learning')
 
-    query ="SELECT * FROM ( SELECT RANK() OVER( ORDER BY NumDealsPurchases DESC, Income DESC ) AS TOP_3_customers, ID, Income, NumDealsPurchases FROM customer_campaign ) AS tmp WHERE tmp.TOP_3_customers <= 3;"
+    query = "SELECT COUNT(ID) as customers, AcceptedCmp1, MntMeatProducts, MntFishProducts, MntSweetProducts, MntGoldProds FROM customer_campaign GROUP BY AcceptedCmp1;"
+
     df = pd.read_sql(sql=query,con=connection)
 
     target_file_path = "/".join([target_path, file_name])
