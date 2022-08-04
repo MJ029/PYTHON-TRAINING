de91452b9980d056ce1f4bc777fe4dabd22633b3
Jayasurya S <jsurya2552000@gmail.com>
2022-08-04 14:16:29 +0530

    17

--- core/CPA/a.py
+++ core/CPA/a.py
@@ -13,7 +13,7 @@ def create_non_existing_dir(dir_path: str):
     return abs_path
 
 
-file_name = "16"
+file_name = "17"
 target_path = "../../PANDAS_SQL"
 
 
@@ -21,7 +21,7 @@ if __name__ == "__main__":
 
     connection = ctr.connect(user='root', host='localhost', password='blackforest', database='sql_learning')
 
-    query = "SELECT * FROM ( SELECT RANK() OVER( ORDER BY Income DESC ) AS TOP_3_customers, ID, Income FROM customer_campaign ) AS tmp WHERE tmp.TOP_3_customers <= 3;"
+    query = "SELECT * FROM ( SELECT RANK() OVER( ORDER BY Income ASC ) AS LAST_3_customers, ID, Income FROM customer_campaign ) AS tmp LIMIT 3;"
 
     df = pd.read_sql(sql=query,con=connection)
 
