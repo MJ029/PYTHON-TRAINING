7c0edae85076c82fcae3df1edab58c9a03d16692
Jayasurya S <jsurya2552000@gmail.com>
2022-08-04 13:56:23 +0530

    7

--- core/CPA/a.py
+++ core/CPA/a.py
@@ -13,7 +13,7 @@ def create_non_existing_dir(dir_path: str):
     return abs_path
 
 
-file_name = "customer_campaign"
+file_name = "MRF"
 target_path = "../../PANDAS_SQL"
 
 
@@ -21,50 +21,10 @@ if __name__ == "__main__":
 
     connection = ctr.connect(user='root', host='localhost', password='blackforest', database='sql_learning')
 
-    Table = {}
+    query = "SELECT education AS MRF, COUNT(education) AS `customers` FROM customer_campaign GROUP BY education ORDER BY `customers` DESC LIMIT 1;"
 
-    """CREATE TABLE IF NOT EXISTS 'customer_campaign'
-    SELECT c.ID,
-    c.KidHome
-    SELECT c.ID,
-    STR_TO_DATE(c.Dt_customer, '%d-%m-%Y') AS join_date,
-    STR_TO_DATE (CONCAT(c.Year_Birth, '-01-01'), '%Y-%m-%d') AS YOB,
-    TRIM(c.Education) AS education,
-    TRIM(c.Marital_Status) AS martial_status,
-    CAST(c.Income AS DECIMAL(28, 2)) AS Income
-    ,c.Teenhome
-    ,c.Recency
-    ,c.Complain
-    ,p.NumWebPurchases
-    ,p.NumCatalogPurchases
-    ,p.NumStorePurchases
-    ,p.NumWebVisitsMonth
-    ,q.MntWines
-    ,q.MntFruits
-    ,q.MntMeatProducts
-    ,q.MntFishProducts
-    ,q.MntSweetProducts
-    ,q.MntGoldProds
-    ,r.NumDealsPurchases
-    ,r.AcceptedCmp1
-    ,r.AcceptedCmp2
-    ,r.AcceptedCmp2
-    ,r.AcceptedCmp3
-    ,r.AcceptedCmp4
-    ,r.AcceptedCmp5
-    ,r.Response
-    FROM customer_details AS c
-    JOIN places_details AS p ON c.ID = p.ID
-    JOIN product_details AS q ON c.ID = q.ID
-    JOIN promotions_details AS r ON c.ID = r.ID;"""
+    df = pd.read_sql(sql=query,con=connection)
 
-    table_name = "customer_campaign"
-
-    query = f"SELECT * FROM {table_name}"
-
-    df = pd.read_sql(sql=query, con=connection)
-
-    # df = pd.read_sql_table(table_name='customer_campaign', con=connection)
     target_file_path = "/".join([target_path, file_name])
 
     target_file_abs_path = create_non_existing_dir(dir_path=target_file_path)
@@ -72,4 +32,5 @@ if __name__ == "__main__":
     complete_file_path = "/".join([target_file_abs_path, f"{file_name}.csv"])
 
     df.to_csv(complete_file_path, index=False)
+
 
