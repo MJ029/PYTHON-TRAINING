391b01ac6597a129ee71d15062426902f5321e27
Jayasurya S <jsurya2552000@gmail.com>
2022-08-04 14:00:40 +0530

    9

--- core/CPA/a.py
+++ core/CPA/a.py
@@ -13,7 +13,7 @@ def create_non_existing_dir(dir_path: str):
     return abs_path
 
 
-file_name = "8"
+file_name = "9"
 target_path = "../../PANDAS_SQL"
 
 
@@ -21,7 +21,7 @@ if __name__ == "__main__":
 
     connection = ctr.connect(user='root', host='localhost', password='blackforest', database='sql_learning')
 
-    query = "SELECT ID, join_date FROM customer_campaign WHERE join_date <= '2013-01-01';"
+    query = "SELECT ID, join_date FROM customer_campaign WHERE join_date >='2014-01-01';"
 
     df = pd.read_sql(sql=query,con=connection)
 
